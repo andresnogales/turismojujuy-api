ee20651d9a31a0c4673ed8b93355ce0904956efe
Andrés Nogales <alejandroandresnogales@gmail.com>
2022-01-26 21:32:42 -0300

    project creation

---
+++ src/main/java/com/andresnogales/turismojujuy/TurismojujuyApplication.java
@@ -0,0 +1,13 @@
+package com.andresnogales.turismojujuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismojujuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurismojujuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andresnogales/turismojujuy/TurismojujuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresnogales.turismojujuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismojujuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
