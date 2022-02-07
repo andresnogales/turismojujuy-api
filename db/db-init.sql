0f43a41679ebdeccb8b8cea7873cfaa3c3e0be02
Andrés Nogales <alejandroandresnogales@gmail.com>
2022-02-07 06:23:16 -0300

    add database init script

---
+++ db/db-init.sql
@@ -0,0 +1,108 @@
+-- public.categories definition
+
+-- Drop table
+
+-- DROP TABLE public.categories;
+
+CREATE TABLE public.categories (
+	id serial4 NOT NULL,
+	title varchar(100) NOT NULL,
+	title_en varchar(100) NULL,
+	CONSTRAINT categories_pk PRIMARY KEY (id)
+);
+
+
+-- public.locations definition
+
+-- Drop table
+
+-- DROP TABLE public.locations;
+
+CREATE TABLE public.locations (
+	id serial4 NOT NULL,
+	"name" varchar(50) NOT NULL,
+	description text NULL,
+	description_en text NULL,
+	region varchar(20) NOT NULL,
+	latitude float8 NULL,
+	longuitude float8 NULL,
+	CONSTRAINT locations_pk PRIMARY KEY (id)
+);
+
+
+-- public.users definition
+
+-- Drop table
+
+-- DROP TABLE public.users;
+
+CREATE TABLE public.users (
+	id serial4 NOT NULL,
+	create_time information_schema."time_stamp" NULL,
+	"name" varchar(100) NOT NULL,
+	"password" varchar(100) NOT NULL,
+	"role" varchar(20) NOT NULL,
+	username varchar(100) NOT NULL,
+	email varchar(150) NOT NULL,
+	CONSTRAINT users_pk PRIMARY KEY (id),
+	CONSTRAINT users_un UNIQUE (username, email)
+);
+
+
+-- public.articles definition
+
+-- Drop table
+
+-- DROP TABLE public.articles;
+
+CREATE TABLE public.articles (
+	id serial4 NOT NULL,
+	title varchar(100) NOT NULL,
+	title_en varchar(100) NULL,
+	description varchar NOT NULL,
+	description_en varchar NULL,
+	picture varchar(200) NULL,
+	address varchar(100) NULL,
+	latitude float8 NULL,
+	longitude float8 NULL,
+	featured bool NULL,
+	location_id int4 NULL,
+	CONSTRAINT articles_pk PRIMARY KEY (id),
+	CONSTRAINT article_location_fk FOREIGN KEY (location_id) REFERENCES public.locations(id)
+);
+
+
+-- public.events definition
+
+-- Drop table
+
+-- DROP TABLE public.events;
+
+CREATE TABLE public.events (
+	id serial4 NOT NULL,
+	title varchar(100) NOT NULL,
+	title_en varchar(100) NULL,
+	description varchar NULL,
+	description_en text NULL,
+	"begin" date NULL,
+	"end" date NULL,
+	picture varchar(200) NULL,
+	location_id int4 NULL,
+	CONSTRAINT events_location_fk FOREIGN KEY (location_id) REFERENCES public.locations(id)
+);
+
+
+-- public.article_categories definition
+
+-- Drop table
+
+-- DROP TABLE public.article_categories;
+
+CREATE TABLE public.article_categories (
+	id serial4 NOT NULL,
+	article_id int4 NULL,
+	category_id int4 NOT NULL,
+	CONSTRAINT article_categories_pk PRIMARY KEY (id),
+	CONSTRAINT article_fk FOREIGN KEY (article_id) REFERENCES public.articles(id) ON DELETE CASCADE,
+	CONSTRAINT categories_fk FOREIGN KEY (category_id) REFERENCES public.categories(id)
+);
