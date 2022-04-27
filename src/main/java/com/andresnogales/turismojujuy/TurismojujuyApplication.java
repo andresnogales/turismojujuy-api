23d030c9c5d64200f05eeb8ea232a2da1f643382
Andrés Nogales <alejandroandresnogales@gmail.com>
2022-04-27 05:20:05 -0300

    add global cors config

--- src/main/java/com/andresnogales/turismojujuy/TurismojujuyApplication.java
+++ src/main/java/com/andresnogales/turismojujuy/TurismojujuyApplication.java
@@ -2,6 +2,9 @@ package com.andresnogales.turismojujuy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -11,4 +14,13 @@ public class TurismojujuyApplication {
 		SpringApplication.run(TurismojujuyApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://deploy--turismojujuy-frontend.netlify.app");
+			}
+		};
+	}
 }
